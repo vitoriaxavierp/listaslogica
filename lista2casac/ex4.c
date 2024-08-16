f08e93096b8dede428cd098e579816f8a57f9aaa
Vitória Xavier <vitoriapxaviera@gmail.com>
2024-08-16 10:27:05 -0300

    Lista 2 Casa em C

--- lista2casac/ex4.c
+++ lista2casac/ex4.c
@@ -1,42 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    float n1, n2, n3;
-   printf("Digite o primeiro número:");
-   scanf("%f", &n1);
-   printf("Digite o segundo número:");
-   scanf("%f", &n2);
-   printf("Digite o terceiro número:");
-   scanf("%f", &n3);
-   
-   if(n1>=n2>=n3){
-       printf("A sequência é: %f,%f,%f", n1, n2, n3);
-   }
-   else{
-       if(n2>=n1>=n3){
-           printf(" A sequência é: %f,%f,%f", n2, n1, n3);
-       }
-       else{
-           if(n1>=n3>=n2){
-               printf(" A sequência é: %f,%f,%f", n1, n3, n2);
-           }
-           else{
-               if(n2>n3>n1){
-                   printf(" A sequência é: %f,%f,%f", n2, n3, n1);
-               }
-               else{
-                   if(n3>n1>n2){
-                       printf(" A sequência é: %f,%f,%f", n3, n1, n2);
-                   }
-                   else{
-                       if(n3>n2>n1){
-                           printf(" A sequência é: %f,%f,%f", n3, n2, n1);
-                       }
-                   }
-               }
-           }
-       }
-   }
-   
+    int n1, n2, n3;
+    printf("Digite o valor de n1: ");
+    scanf("%i", &n1);
+    
+    printf("Digite o valor de n2: ");
+    scanf("%i", &n2);
+    
+    printf("Digite o valor de n3: ");
+    scanf("%i", &n3);
+
+    if (n1 <= n2 && n2 <= n3) {
+        printf("Valores em ordem crescente: %i, %i, %i", n1, n2, n3);
+    } else if (n1 <= n3 && n3 <= n2) {
+        printf("Valores em ordem crescente: %i, %i, %i", n1, n3, n2);
+    } else if (n2 <= n1 && n1 <= n3) {
+        printf("Valores em ordem crescente: %i, %i, %i", n2, n1, n3);
+    } else if (n2 <= n3 && n3 <= n1) {
+        printf("Valores em ordem crescente: %i, %i, %i", n2, n3, n1);
+    } else if (n3 <= n1 && n1 <= n2) {
+        printf("Valores em ordem crescente: %i, %i, %i", n3, n1, n2);
+    } else {
+        printf("Valores em ordem crescente: %i, %i, %i", n3, n2, n1);
+    }
+
     return 0;
 }
